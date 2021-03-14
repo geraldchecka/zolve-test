606f425556c8fa4d7582aac1683767b0d5f86928
Anvesh Checka <anvesh.checka@gmail.com>
2021-03-14 18:14:14 +0530

    bootstrapping the project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+
+function Root() {
+  return (
+    <p>Zolve App Test</p>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById("app-root"));
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    compress: true,
+    host: "0.0.0.0",
+    port: 9001,
+    open: true,
+    disableHostCheck: true
+  },
+  stats: {
+    warnings: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Zolve Test",
+      filename: "index.html",
+      template: "./src/index.html"
+    })
+  ]
+};
