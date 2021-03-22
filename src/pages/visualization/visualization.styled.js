5839334f97697960a306220d37110eaf5f9f4186
Anvesh Checka <anvesh.checka@gmail.com>
2021-03-22 11:57:29 +0530

    visualization code cleanup

--- src/pages/visualization/visualization.styled.js
+++ src/pages/visualization/visualization.styled.js
@@ -5,29 +5,19 @@ export const Container = styled.div``;
 export const InputWrapper = styled.div`
   display: inline-block;
   width: 100%;
-
 `;
 
 export const InputControl = styled.div`
-  // display: flex;
-  // flex-direction: row;
-  // flex-basis: auto;
   display: inline-block;
 `;
 
 export const Label = styled.div`
-  // display: flex;
-  // flex-direction: column;
-  // flex-basis: auto;
   display: inline-block;
   width: 100px;
   text-align: right;
 `;
 
 export const Input = styled.input`
-  // display: flex;
-  // flex-direction: column;
-  // flex-basis: auto;
   display: inline-block;
   width: 150px;
   text-align: left;
