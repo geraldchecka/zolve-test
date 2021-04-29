b8e091b0f890f658fcd1d65dc5552c696ca56e4e
Anvesh Checka <anvesh.checka@gmail.com>
2021-04-29 21:51:12 +0530

    wrote the basic error object creation function

--- src/globals/utils.js
+++ src/globals/utils.js
@@ -1,3 +1,8 @@
+/**
+ * Serializes all the URL query parameters
+ * @param {Object} obj A key-value pair of all URL parameters
+ * @returns 
+ */
 export const serialize = function(obj) {
   var str = [];
   for (var p in obj)
@@ -7,7 +12,49 @@ export const serialize = function(obj) {
   return str.join("&");
 }
 
+/**
+ * Checks a string against a valid URL regex
+ * @param {String} str A string to compare against
+ * @returns {Boolean} Whether it is a valid URL or not.
+ */
 export const validUrl = function(str) {
   const urlRegex = /(?:^|\s)((https?:\/\/)?(?:localhost|[\w-]+(?:\.[\w-]+)+)(:\d+)?(\/\S*)?)/;
   return urlRegex.test(str);
+}
+
+/**
+ * Formats and returns an error object for consumption with UI layer
+ * @param {Object} error Contains the error object
+ * @returns {Object}
+ */
+export const generateError = function(error) {
+  let errorType = {
+    name: "",
+    message: ""
+  };
+
+  function setErrorType(name = "", msg = "") {
+    errorType.name = error.name;
+    errorType.message = error.message;
+  }
+
+  switch(error.name) {
+    case "NotAllowedError":
+      console.error(error.message);
+      setErrorType(error.name, error.message);
+      break;
+    case "OverconstrainedError":
+      console.error(error.message);
+      setErrorType(error.name, error.message);
+      break;
+    default:
+      console.error(
+        error.name,
+        error.message,
+        "https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia#exceptions"
+      );
+      setErrorType(error.name, error.message);
+      break;
+  }
+  return errorType;
 }
