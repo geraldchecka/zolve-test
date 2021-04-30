406cf8b201f42d7bdb4975d393fdc3f15ecb6942
Anvesh Checka <anvesh.checka@gmail.com>
2021-04-30 23:33:34 +0530

    Change to wrapper with additional dependencies

--- src/pages/camera/index.jsx
+++ src/pages/camera/index.jsx
@@ -3,7 +3,9 @@ import { Container, Button } from './camera.styled';
 import useCamera from "./useCamera";
 
 export default function Camera() {
-  const cameraRef = useCamera({});
+  const cameraRef = useCamera();
+  // Load a spinner until all the components have finished loading
+  // Probaly a pipiline to detect loading state of all components in the tree.
 
   // Run the code within an iframe with security permissions
   return (
