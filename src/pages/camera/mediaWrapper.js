f1a288c125336075fe93c2fb2d863eb2f4e84f83
Anvesh Checka <anvesh.checka@gmail.com>
2021-04-30 23:34:41 +0530

    Better exception handling, abstracted some code

--- src/pages/camera/mediaWrapper.js
+++ src/pages/camera/mediaWrapper.js
@@ -6,10 +6,39 @@ import { generateError } from '../../globals/utils';
  * @property {boolean} status - Denotes whether the corresponding 'type' is supported or not
  * @property {string} type    - What type of API is supported
  */
-let supports = {
-  status: false,
-  type: ""
+const supports = {
+  getUserMedia: {
+    status: false,
+    type: ""
+  },
+  enumerateDevices: {
+    status: false,
+    type: "modern",
+  }
 };
+
+/**
+ * An object containing basic constraints supported by all popular browsers.
+ * @returns {Object} Contains the necessary constraints
+ */
+ const getConstraints = function() {
+  return {
+    video: {
+      width: {
+        min: 640,
+        max: 1280,
+        ideal: 640,
+      },
+      height: {
+        min: 480,
+        max: 720,
+        ideal: 480,
+      },
+      facingMode: "user"
+    },
+    audio: true,
+  };
+}
 
 /**
  * As part of feature detection, generalize the cross-browser mediaDevices API.
@@ -25,14 +54,36 @@ const patchMediaDevices = function() {
   * @returns {undefined} Updates the supports namespace object
   */
 const checkBrowserSupport = function() {
+  // Check for getUserMedia
   if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-    supports.status = true;
-    supports.type = "modern";
+    supports.getUserMedia.status = true;
+    supports.getUserMedia.type = "modern";
   }
   else if ('getUserMedia' in navigator) {
-    supports.status = true;
-    supports.type = "legacy";
+    supports.getUserMedia.status = true;
+    supports.getUserMedia.type = "legacy";
   }
+
+  // Check for enumerateDevices
+  if ('mediaDevices' in navigator && 'enumerateDevices' in navigator.mediaDevices) {
+    supports.enumerateDevices.status = true;
+    supports.enumerateDevices.type = "modern";
+  }
+}
+
+/**
+ * Method to check if an API is supported by the browser. Only getUserMedia dn enumerateDevices are supported by this public method.
+ * @param {String} query Parameter to be searched on
+ * @return {Array[Boolean, String]} Returns an array containing two values. boolean on the query. On an unsuccessful query, undefined and query are returned.
+ */
+const isSupported = function(query = "") {
+  if (query === "getUserMedia") {
+    return [supports.getUserMedia.status, supports.getUserMedia.type];
+  }
+  else if (query === "enumerate") {
+    return [supports.enumerateDevices.status, supports.enumerateDevices.type];
+  }
+  return [undefined, query];
 }
 
 /**
@@ -40,14 +91,22 @@ const checkBrowserSupport = function() {
  * @returns {Object} Contains list of devices segregated as audio or video.
  */
 export const getAllDevices = async function() {
-  let devices = await navigator.mediaDevices.enumerateDevices();
-
+  const [status, type] = isSupported("getUserMedia");
+  let devices;
+  
   function getTemplate(id, groupId, label) {
     return {
       id,
       groupId,
       label,
     };
+  }
+
+  try {
+    devices = await navigator.mediaDevices.enumerateDevices();
+  }
+  catch(e) {
+    devices = [];
   }
 
   return devices.reduce((acc, { deviceId, groupId, kind, label }, _idx, items) => {
@@ -75,32 +134,45 @@ export const getAllDevices = async function() {
   });
 }
 
-export const getUserMedia = async function (constraints) {
+/**
+ * This function is an abstraction for getUserMedia, to support both modern and legacy browsers.
+ * @param {Object} constraints Contains all the constraints that will be supported
+ * @returns {Object} Promise
+ */
+export const getUserMedia = async function (constraints = getConstraints()) {
   let returnPromise;
   
   returnPromise = new Promise((resolve, reject) => {
+    const [status, type] = isSupported("getUserMedia");
+
     function successCB(response) {
+      console.log(response);
       resolve(response);
     }
 
     function failureCB(error) {
-      console.log(error);
+      console.error(error);
       reject(generateError(error));
     }
 
-    if (supports.type === "legacy" && supports.status === true) {
-      navigator.getUserMedia(constraints, successCB, failureCB);
+    try {
+      if (type === "legacy" && status === true) {
+        navigator.getUserMedia(constraints, successCB, failureCB);
+      }
+      else if (type === "modern" && status === true) {
+        navigator.mediaDevices.getUserMedia(constraints)
+          .then(successCB)
+          .catch(failureCB);
+      }
     }
-    else if (supports.type === "modern" && supports.status === true) {
-      navigator.mediaDevices.getUserMedia(constraints)
-        .then(successCB)
-        .catch(failureCB);
+    catch(e) {
+      reject(generateError(e));
     }
   });
 
   return returnPromise;
 }
 
-checkBrowserSupport();
-
+// Do this immediately on as soon as the module loads
 patchMediaDevices();
+checkBrowserSupport();
